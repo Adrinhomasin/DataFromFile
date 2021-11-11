06b304245951c3c6d076c06c88dc379be38f7ed5
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:26:29 +0200

    DataFromFile

---
+++ Chuck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\users\opilane\samples";
+            string fileName = "Chuck.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            Random rnd = new Random();
+            Console.WriteLine(dataFromFile[rnd.Next(dataFromFile.Length)]);
+        }
+    }
+}
---
+++ DataFromFile/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color");
+            string userColor = Console.ReadLine();
+            string output = GetLineFromFileData(userColor);
+
+            if(output.Length != 0)
+            {
+                Console.WriteLine(output);
+                
+            }
+            else
+            {
+                Console.WriteLine("sorry color not found");
+            }
+            
+            
+        }
+     
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\users\opilane\samples";
+            string fileName = "personality.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            return dataFromFile;
+        }
+        private static string GetLineFromFileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+            foreach(string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+                
+            }
+            return result;
+        }
+    }
+
+}
